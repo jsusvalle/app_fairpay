70d1ed5244d1c577e4f6bc0aa71e023ffb18bf72
Jsusvalle <jesusvalle2001@hotmail.com>
2023-01-19 21:59:42 -0500

    feat: add tailwind css

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,22 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: ["nativewind/babel",
+      [
+        'module-resolver',
+        {
+          alias: {
+            app: './src/app',
+            components: './src/components',
+            hooks: './src/hooks',
+            infrastructure: './src/infrastructure',
+            models: './src/models',
+            context: './src/context',
+            mock_services: './src/mock_services',
+            utils: './src/utils',
+          },
+        },
+      ],
+    ],
   };
 };
